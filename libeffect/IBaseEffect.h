00dee9593d88474e0bea02c10a575476e962aac3
liyongfa <liyongfa@kugou.net>
2015-12-19 18:18:34 +0800

    添加图像效果模块

--- libeffect/IBaseEffect.h
+++ libeffect/IBaseEffect.h
@@ -4,6 +4,7 @@ namespace e
 	class IBaseEffect
 	{
 	public:
+		virtual void SetType(int nType) = 0;
 		virtual void OnSampleProc(void* pData, int nSize, int nWidth, int nHeight, int nBitCount) = 0;
 	};
 }
